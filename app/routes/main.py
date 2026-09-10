852749aabe9efa38ec37299d183011ace1cfa3d6
adielvillarureta <i2511911@continental.edu.pe>
2026-09-10 08:30:51 -0500

    estructura nueva lista

--- app/routes/main.py
+++ app/routes/main.py
@@ -8,4 +8,18 @@ import plotly, json
 import plotly.graph_objs as go
 from sqlalchemy import func, text
 
-main_bp = Blueprint('main', __name__)   # ← ESTA LÍNEA ES CLAVE
+main_bp = Blueprint('main', __name__)
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def inicio():
+    return render_template('index.html')
+
+@main_bp.route('/acerca-de')
+def acerca_de():
+    return render_template('acerca_de.html')
+
+@main_bp.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
